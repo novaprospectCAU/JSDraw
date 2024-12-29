018ba6271329e96f57bdab04ba6a91be0a714bc2
novaprospectCAU <novaprospect110a@gmail.com>
2024-12-29 22:55:39 +0900

    그림을 그리는 중인지를 체크하는 부울 변수를 추가했습니다.

--- srcs/canvas/canvas.js
+++ srcs/canvas/canvas.js
@@ -8,7 +8,117 @@ class DrawingInfo {
   constructor() {
     this.drawingMode = "pen";
     this.currentColor = "hsl(180deg, 0%, 0%)";
+    this.isDrawing = false;
   }
 }
 
 export const drawingInfo = new DrawingInfo();
+
+CANVAS.addEventListener("mousedown", (e) => {
+  drawingInfo.isDrawing = true;
+});
+
+CANVAS.addEventListener("mousemove", (e) => {
+  if (drawingInfo.isDrawing) {
+    switch (drawingInfo.drawingMode) {
+      case "pen": {
+        break;
+      }
+      case "eraser": {
+        break;
+      }
+      case "spray": {
+        break;
+      }
+      case "brush": {
+        break;
+      }
+      case "ink": {
+        break;
+      }
+      case "text": {
+        break;
+      }
+      case "line": {
+        break;
+      }
+      case "curve": {
+        break;
+      }
+      case "rectangle": {
+        break;
+      }
+      case "polygon": {
+        break;
+      }
+      case "oval": {
+        break;
+      }
+      case "select rect": {
+        break;
+      }
+      case "select poly": {
+        break;
+      }
+      case "magnify": {
+        break;
+      }
+      default: {
+        console.error("도구 선택 오류");
+      }
+    }
+  }
+});
+
+CANVAS.addEventListener("mouseup", (e) => {
+  if (drawingInfo.isDrawing) {
+    drawingInfo.isDrawing = false;
+    switch (drawingInfo.drawingMode) {
+      case "pen": {
+        break;
+      }
+      case "eraser": {
+        break;
+      }
+      case "spray": {
+        break;
+      }
+      case "brush": {
+        break;
+      }
+      case "ink": {
+        break;
+      }
+      case "text": {
+        break;
+      }
+      case "line": {
+        break;
+      }
+      case "curve": {
+        break;
+      }
+      case "rectangle": {
+        break;
+      }
+      case "polygon": {
+        break;
+      }
+      case "oval": {
+        break;
+      }
+      case "select rect": {
+        break;
+      }
+      case "select poly": {
+        break;
+      }
+      case "magnify": {
+        break;
+      }
+      default: {
+        console.error("도구 선택 오류");
+      }
+    }
+  }
+});
