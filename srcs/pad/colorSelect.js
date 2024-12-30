646e87b1ef33cec30df212c90eb6be6d06fd8d74
novaprospectCAU <novaprospect110a@gmail.com>
2024-12-30 15:31:06 +0900

    palette 위치 버그를 수정했습니다.

--- srcs/pad/colorSelect.js
+++ srcs/pad/colorSelect.js
@@ -1,9 +1,17 @@
+export const COLOR_PALETTE = document.getElementById("palette");
+export const COLOR_PALETTE_CTX = COLOR_PALETTE.getContext("2d");
 export const COLOR_PICKER = document.getElementById("picker");
 export const COLOR_PICKER_CTX = COLOR_PICKER.getContext("2d");
 
 export function colorInit() {
+  COLOR_PALETTE.setAttribute("width", window.innerWidth);
+  COLOR_PALETTE.setAttribute("height", window.innerHeight);
+  COLOR_PALETTE.style.width = `${window.innerWidth}px`;
+  COLOR_PALETTE.style.height = `${window.innerHeight}px`;
   COLOR_PICKER.setAttribute("width", window.innerWidth);
   COLOR_PICKER.setAttribute("height", window.innerHeight);
   COLOR_PICKER.style.width = `${window.innerWidth}px`;
   COLOR_PICKER.style.height = `${window.innerHeight}px`;
 }
+
+COLOR_PICKER_CTX;
