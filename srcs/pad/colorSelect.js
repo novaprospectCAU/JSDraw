2c9d20c56cebf96f227cb5508470155f4d19861a
novaprospectCAU <novaprospect110a@gmail.com>
2024-12-30 16:01:47 +0900

    palette에 색상을 추가했습니다. 단, hue만 고려했으므로, s,l도 추가해야 합니다.

--- srcs/pad/colorSelect.js
+++ srcs/pad/colorSelect.js
@@ -12,4 +12,29 @@ export function colorInit() {
   COLOR_PICKER.setAttribute("height", window.innerHeight);
   COLOR_PICKER.style.width = `${window.innerWidth}px`;
   COLOR_PICKER.style.height = `${window.innerHeight}px`;
+
+  paletteInit();
+}
+
+export function paletteInit() {
+  const pWIDTH = COLOR_PALETTE.width;
+  const pHEIGHT = COLOR_PALETTE.height;
+  const pCenterX = pWIDTH / 2;
+  const pCenterY = pHEIGHT / 2;
+  const RADIAN = Math.PI / 180;
+
+  for (let angle = 0; angle < 360; angle++) {
+    COLOR_PALETTE_CTX.beginPath();
+    COLOR_PALETTE_CTX.moveTo(pCenterX, pCenterY);
+    COLOR_PALETTE_CTX.arc(
+      pCenterX,
+      pCenterY,
+      pWIDTH,
+      angle * RADIAN,
+      (angle + 1) * RADIAN
+    );
+    COLOR_PALETTE_CTX.closePath();
+    COLOR_PALETTE_CTX.fillStyle = `hsl(${(angle + 90) % 360}, 100%, 50%)`;
+    COLOR_PALETTE_CTX.fill();
+  }
 }
